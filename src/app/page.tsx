e86002c001e0afc4c044b9f039e8081e947c26f0
venlomj <venlo.mj@hotmail.nl>
2025-01-29 22:30:18 +0100

    ui configurated.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import ThemeToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
-    <div className="">
-      <h1 className="font-bold text-blue-500">Welcome to the course</h1>
+    <div className="p-5">
+      <div className="w-100 flex justify-end">
+        <ThemeToggle />
+      </div>
+      <h1 className="text-blue-500 font-barlow">Welcome to the course</h1>
+      <Button variant="destructive">Click here</Button>
     </div>
   );
 }
